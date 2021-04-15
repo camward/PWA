a0b2e35653d27bf2aea7c9293209aff4ff97d8f0
alex <camward@yandex.ru>
2021-04-15 21:12:01 +0400

    статический кэш

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
+const staticCacheName = "s-app-v1";
+
+const assetUrls = ["index.html", "/js/app.js", "/css/styles.css"];
+
 self.addEventListener("install", async (event) => {
-  console.log("SW - install");
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls);
 });
 
 self.addEventListener("activate", async (event) => {
